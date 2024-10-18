13d1afd4a8055ca4bbecd288dba0fe55cb17deb5
Mayank Manjhi <Mayank.manjhi7@gmail.com>
2024-10-18 18:43:01 +0530

    setup and require again

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## this is my project ##
 ## let star my project ##
+3.8.0
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages , setup
+from typing import List
+
+
+
+
+
+
+
+
+
+setup (
+name='sensors',
+version="0.0.1",
+author="mm",
+packages= find_packages(),
+install_requires = ["pymongo"]
+
+
+
+)
