f675b118f0d8aece0efdfdd02709b64c0da98573
Mayank Manjhi <Mayank.manjhi7@gmail.com>
2024-10-19 17:11:02 +0530

    exception and logging done

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import find_packages , setup
 from typing import List
 
+def get_requirements()->list[str]:
 
+    requirement_list=list[str] =[]
+   
+    return requirement_list
 
 
 
@@ -14,7 +18,7 @@ name='sensors',
 version="0.0.1",
 author="mm",
 packages= find_packages(),
-install_requires = ["pymongo"]
+install_requires = get_requirements(), #["pymongo"]
 
 
 
